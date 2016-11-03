ba0344443dcf56dc829574a1efd452d1f042f988
Andriy Zabolotnyy <andriymz@hotmail.com>
2016-11-03 17:50:41 +0000

    Ex 4 updated

--- project1/exercise-4.py
+++ project1/exercise-4.py
@@ -1,8 +1,8 @@
-import re
 import os
 import math
 import operator
-
+import nltk
+from nltk.util import ngrams
 from nltk.corpus import stopwords
 
 def readDocument(docName, directory=os.path.join(os.path.dirname(__file__), os.pardir, "resources\\")):
@@ -16,68 +16,115 @@ def readDocument(docName, directory=os.path.join(os.path.dirname(__file__), os.p
 
     return no_punctuation
 
-def tokenize(t):
+def prepareDocuments(documents):
+    """
+    Tokenizes, removes stopwords and punctuation
 
-    text = t.lower()
-    text = re.sub("\n"," ",text)
-    text = re.sub(r'<[^>]+>',"",text) # remove all html markup
-    text = re.sub('[^a-zèéeêëûüùôöòóœøîïíàáâäæãå&@#A-Z0-9- \']', "", text)
-    wrds = text.split()
-    return wrds
+    :param documents: list of strings (documents)
+    :return: list of list of strings (each documents terms)
+    """
+    prepared = [nltk.word_tokenize(d) for d in documents]
+    prepared = [removeStopWords(d) for d in prepared]
+    # TODO remove punctuation
 
-def getAllNgrams(text,maxn) :
+    return prepared
 
-    words = tokenize(text)
-    i=0
-    terms = dict()
+def removeStopWords(list_terms):
+    return [token for token in list_terms if token not in stopwords.words('english')]
 
-    for word in words :
-        if word not in stopwords.words('english') and len(word) > 1 and '@' not in word:
-            if word in terms :
-                terms[word] += 1
-            else :
-                terms[word] = 1
-        if maxn >= 2 :
-            if i< len(words)-1 :
-                if words[i] not in stopwords.words('english') and words[i+1] not in stopwords.words('english'):
-                    bigram = words[i]+ " " +words[i+1]
-                    if bigram in terms :
-                        terms[bigram] += 1
-                    else :
-                        terms[bigram] = 1
+def getWordGrams(words, min=1, max=3):
+    """
+    Getting n-grams in a specified range
+    """
 
-                if maxn >= 3 :
-                    if i < len(words)-2 :
-                        if not words[i] in stopwords.words('english') and not words[i+2] in stopwords.words('english'):
-                            # middle word can be a stopword
-                            trigram = words[i]+ " " +words[i+1]+ " " +words[i+2]
-                            if trigram in terms :
-                                terms[trigram] += 1
-                            else :
-                                terms[trigram] = 1
-        i += 1
-    return terms
+    s = []
 
-def read_text_in_dict(text):
+    for n in range(min, max):
+        for ngram in ngrams(words, n):
+            s.append(' '.join(str(i) for i in ngram))
 
-    freq_dict = getAllNgrams(text,3)
-    total_term_count = 0
+    return s
 
-    for key in freq_dict:
-        total_term_count += freq_dict[key]
+def buildTermCountDict(doc_list):
 
-    return freq_dict, total_term_count
+    freq_dict = {}
 
-def print_top_n_terms(score_dict,n):
+    for doc in doc_list:
+        n_grams = getWordGrams(doc)
 
-    sorted_terms = sorted(score_dict.items(),key=operator.itemgetter(1),reverse=True)
-    i=0
+        for term in n_grams:
+            if term in freq_dict:
+                freq_dict[term] = freq_dict[term] + 1
+            else:
+                freq_dict[term] = 1
 
-    for (term,score) in sorted_terms:
-        i += 1
-        print(term,score)
-        if i==n:
-            break
+    total_unigram_count = 0
+
+    for term in freq_dict:
+        if ' ' not in term:
+            total_unigram_count += freq_dict[term]
+
+    return freq_dict, total_unigram_count
+
+def calcCandidateKLDivScore(fg_term_freq, bg_term_freq, fg_unigram_count, bg_unigram_count):
+    """
+    Calculate KLDiv per term (unigram and bigram) from foreground document
+
+    :param foreground_term_freq: dictionary holding frequency of each term in foreground document
+    :return:  dictionary with score ( KLDiv = KlDivInformativeness + KlDivPhraseness ) per term
+    """
+
+    kldiv_per_term = {}
+
+    for term in fg_dict:
+
+        fg_unigram_term_probability = calcLM1(term, fg_term_freq, fg_unigram_count)
+        bg_unigram_term_probability = calcLM1(term, bg_term_freq, bg_unigram_count)
+        bg_bigram_term_probability = calcLM2(term, bg_term_freq, bg_unigram_count)
+
+        klDivInformativeness = fg_unigram_term_probability * math.log(fg_unigram_term_probability / bg_unigram_term_probability)
+        klDivPhraseness = bg_bigram_term_probability * math.log(bg_bigram_term_probability / bg_unigram_term_probability)
+
+        klDiv = klDivInformativeness + klDivPhraseness
+
+        kldiv_per_term[term] = klDiv
+
+    return kldiv_per_term
+
+def calcLM1(term, freq_dict, total_unigram_count):
+
+    unigrams = term.split(" ")
+    relfreq_unigrams = 1.0
+
+    for unigram in unigrams:
+
+        u_freq = 1  # add-1 smoothing
+
+        if unigram in freq_dict:
+            u_freq = freq_dict[unigram]
+
+        u_relfreq = float(u_freq) / float(total_unigram_count)
+        relfreq_unigrams *= u_relfreq
+
+    return relfreq_unigrams
+
+def calcLM2(term, freq_dict, total_unigram_count):  #TODO
+
+    term_freq = 1   # add-1 smoothing
+
+    if term in freq_dict:
+        term_freq = freq_dict[term]
+
+    return float(term_freq) / float(total_unigram_count)
+
+def printTopNTerms(scores, n):
+
+    # reverse ordering of candidates scores
+    top_candidates = sorted(scores.items(), key=lambda x: x[1], reverse=True)
+
+    # top 5 candidates
+    for candidate in top_candidates[:n]:
+        print("" + str(candidate[0]) + " - " + str(candidate[1]))
 
 
 ##################################################################
@@ -86,39 +133,14 @@ def print_top_n_terms(score_dict,n):
 
 #building structure (dict) to hold each term (n-gram) ocurrance number
 #for both foreground and background corpus
-fg_dict, fg_term_count = read_text_in_dict(readDocument("doc_ex4"))
-bg_dict, bg_term_count = read_text_in_dict("backgound text") #TODO background collection
 
-#calculate kldiv per term (n-gram) from foreground document
-kldiv_per_term = {}
+fg = prepareDocuments([readDocument("doc_ex4")])
+bg = prepareDocuments(["palavras lol epa palavras lol epa assim lol epa lol lol epa assim doc1"])
 
-for term in fg_dict:
-    fg_freq = fg_dict[term]
+fg_dict, fg_unigram_count = buildTermCountDict(fg)
+bg_dict, bg_unigram_count = buildTermCountDict(bg) #TODO background collection
 
-    # kldivI is kldiv for informativeness: relative to bg corpus freqs
-    bg_freq = 1
+#KLDiv score per candidate
+scores = calcCandidateKLDivScore(fg_dict, bg_dict, fg_unigram_count, bg_unigram_count)
 
-    if term in bg_dict:
-        bg_freq = bg_dict[term]
-
-    relfreq_fg = float(fg_freq)/float(fg_term_count)
-    relfreq_bg = float(bg_freq)/float(bg_term_count)
-
-    kldivI = relfreq_fg*math.log(relfreq_fg/relfreq_bg)
-
-    # kldivP is kldiv for phraseness: relative to unigram freqs
-    unigrams = term.split(" ")
-    relfreq_unigrams = 1.0
-
-    for unigram in unigrams:
-        if unigram in fg_dict:  # testing because middle n-gram word can be a stop word, so it has no occurance in fg_dict
-            u_freq = fg_dict[unigram]
-            u_relfreq = float(u_freq)/float(fg_term_count)
-            relfreq_unigrams *= u_relfreq
-
-    kldivP = relfreq_fg*math.log(relfreq_fg/relfreq_unigrams)
-    kldiv = kldivI+kldivP
-
-    kldiv_per_term[term] = kldiv
-
-    #print(term,kldivI,kldivP,kldiv)
+printTopNTerms(scores, 5)
