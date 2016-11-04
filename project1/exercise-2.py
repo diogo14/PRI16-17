05a0086db82cb92814a91610215b8c03466b83ff
diogo14 <diogocardoso@outlook.pt>
2016-11-04 12:46:31 +0000

    Created source files

--- project1/exercise-2.py
+++ project1/exercise-2.py
@@ -60,39 +60,27 @@ def print_evaluations(documentNames):
     print "==================="
     print "Mean Av. Precision: " + str(mean_avg_precision(map))
 
-
 def precision(list_r, list_a):
-    # intersection of R with A
     i = set(list_r).intersection(list_a)
-    len2 = len(list_a)
-    if len2 == 0:
+    len_a = len(list_a)
+    if len_a == 0:
         return 0
     else:
-        # relevant docs(R intersection A) within all docs(A)
-        return float(len(i)) / len2
-
+        return float(len(i)) / float(len_a)
 
 def recall(list_r, list_a):
     i = set(list_r).intersection(list_a)
-    # the same but divided by R
-    return float(len(i)) / len(list_r)
+    len_r = len(list_r)
+    if len_r == 0:
+        return 0
+    else:
+        return float(len(i)) / float(len_r)
 
 def f1(prec, rec):
     if prec + rec == 0:
         return 0
     else:
-        return float(2*(prec * rec))/(rec + prec)
-"""
-def f1_score(list_r, list_a):
-    # use above functions
-    prec = precision(list_r, list_a)
-    rec = recall(list_r, list_a)
-    if prec + rec == 0:
-        return 0
-    else:
-        return 2*(prec * rec)/(rec + prec)
-"""
-
+        return float((2* rec * prec))/float((rec + prec))
 
 def avg_precision(list1, list2, k=5):
 
@@ -118,113 +106,9 @@ def mean_avg_precision(ap_list):
         total = total + i
     return float(total) / float(len(ap_list))
 
-#def mean_avg_precision(list1, list2, k=5):
-#    return numpy.mean([avg_precision(l1, l2, k) for l1, l2 in zip(list1, list2)])
-
 ##################################################################
 ## Main starts here
 ##################################################################
+#print_evaluations(getDocumentNames())
 
-#calculateDocumentEvaluation({"text":"text simples"}, ["text simples", "example"])
-print_evaluations(getDocumentNames())
-
-
-
-
-
-"""
-# function document_words
-def document_words(fileglob=os.path.join(os.path.dirname(__file__), "dataset", "documents\\")):
-#def document_words(docName, directory=os.path.join(os.path.dirname(__file__), os.pardir, "documents\\")):
-        #split words and return a dictionary with document and list of words.
-        # cycle for for all documents
-
-    for docname in os.listdir(fileglob):
-        print docname
-"""
-"""
-def document_words(fileglob=os.path.join(os.path.dirname(__file__), "dataset", "documents\\")):
-#def document_words(docName, directory=os.path.join(os.path.dirname(__file__), os.pardir, "documents\\")):
-        #split words and return a dictionary with document and list of words.
-        # cycle for for all documents
-    sets = []
-    idx = 0
-    for docname in os.listdir(fileglob):
-        dict()[idx] = docname
-        f = open(fileglob + docname)
-        read = f.read()
-        read = read.decode('latin-1')
-        words = nltk.word_tokenize(read)
-        sets += [read]
-        idx += 1
-    print(sets)
-
-
-def distinct_keywords(fileglob=os.path.join(os.path.dirname(__file__), "dataset", "indexers\\")):
-#def document_words(docName, directory=os.path.join(os.path.dirname(__file__), os.pardir, "documents\\")):
-        #split words and return a dictionary with document and list of words.
-        # cycle for for all documents
-    sets = []
-    idx = 0
-    for dirpath, dirs, files in os.walk(fileglob):
-        for docname in files:
-            f = open(fileglob + docname)
-            read = f.read()
-            read = read.decode('latin-1')
-            keys = read.split("\n", -1)[:-1]
-            #docname = docname[:-4] + '.txt' could be needed for normalize the extensions of files
-            dict()[docname] = keys
-
-"""
-
-
-
-"""
-for dirpath, dirs, files in os.walk(fileglob):
-        for filname in files:
-            filename = os.listdir(fileglob[0])
-            print filenam
-
-
-
-
-path = '/projec1/dataset/indexers/'
-# function return distinct keywords
-
-
-def distinct_keywords(fileglob=os.path.join(os.path.dirname(__file__), "dataset", "indexers\\")):
-        #retrieve content of each .key file for each document
-    sets = []
-    doc_idex = 0
-    for docname in os.listdir(fileglob)
-
-
-"""
-
-"""
-def distinct_keywords(fileglob=os.path.join(os.path.dirname(__file__), "dataset", "indexers\\")):
-
-    #i = set (list_this).union(list_other)
-    for dirpath, dirs, files in os.walk(fileglob):
-        for filname in files:
-            filename = os.listdir(fileglob[0])
-            print filename
-
-
-"""
-"""
-
-    texts, words = {}, set()
-    for txtfile in glob(fileglob):
-        with open(txtfile, 'r') as f:
-            txt = f.read().split()
-            words |= set(txt)
-            texts[txtfile.split('\\')[-1]] = txt
-    return texts, words
-
-#keywords in each indexer file
-pp(sorted(words))
-#missing check with all documents, maybe update the initial list of keywords.
-
-#def document_keywords(document, words):
-"""
+print recall(["A", "B", "C", "D"], ["A", "E", "F", "B", "Z"])
